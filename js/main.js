58c4b9f001f4876e6013b8beae84fd02b7a9f46e
HtwwtH <alina.l22@mail.ru>
2020-11-03 09:09:14 +0700

    commit1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  const mMenuBtn = $(".mobile-burger");
+  const mMenu = $(".m-menu");
+  const mMenuBtnImg = $(".mobile-burger img");
+
+  mMenuBtn.click(function () {
+    if (mMenu.is(':visible')) {
+      mMenu.slideToggle(600);
+      mMenuBtnImg.css('content', "url('./../img/svg/burger.svg')");
+    }
+    else {
+      mMenu.slideToggle(600);
+      mMenuBtnImg.css('content', "url('./../img/svg/close-m-menu.svg')");
+    }
+  });
+});
